f9fa0225ee6940cadd956728240d137d0bafae97
Miles Necker <asamin@comcast.net>
2022-03-18 19:24:42 -0600

    Initial commit

---
+++ SampleProject/Scripts/MyBlock.cs
@@ -0,0 +1,17 @@
+﻿
+// <property name="Class" value="MyBlock, SampleProject" />
+class MyBlock : Block
+{
+    public override void Init()
+    {
+        Log.Out($"Hello from {GetType()}! Suck mah dick!");
+        base.Init();
+    }
+
+    public override bool OnBlockActivated(int _indexInBlockActivationCommands, WorldBase _world, int _cIdx, Vector3i _blockPos, BlockValue _blockValue, EntityAlive _player) {
+
+        Log.Out("OUCH!");
+        _player.AddHealth(-20);
+        return base.OnBlockActivated(_indexInBlockActivationCommands, _world, _cIdx, _blockPos, _blockValue, _player);
+    }
+}
---
+++ SampleProject/Scripts/MyElectricWire.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class MyElectricWire : BlockElectricWire {
+
+    public override int OnBlockDamaged(WorldBase _world, int _clrIdx, Vector3i _blockPos, BlockValue _blockValue, int _damagePoints, int _entityIdThatDamaged, bool _bUseHarvestTool, bool _bBypassMaxDamage, int _recDepth = 0) {
+
+        Log.Out("Get thrown");
+        _world.GetEntity(_entityIdThatDamaged).transform.Translate(Vector3.forward * 10, Space.World);
+        
+        return base.OnBlockDamaged(_world, _clrIdx, _blockPos, _blockValue, _damagePoints, _entityIdThatDamaged, _bUseHarvestTool, _bBypassMaxDamage, _recDepth);
+    }
+
+}
